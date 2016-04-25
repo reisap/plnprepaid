72e83832739cb778e3d694a906b1f5fc62c2eedd
reisa prasaptaraya <r.prasaptaraya@gmail.com>
2016-04-25 13:23:54 +0700

    Initial Commit

---
+++ PDAMObj/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  PDAMObj
+//
+//  Created by reisa prasaptaraya on 4/25/16.
+//  Copyright © 2016 reisa prasaptaraya. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
