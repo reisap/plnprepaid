5d8c66c747bc0819de575c9bdeebca1f4c669a89
reisa prasaptaraya <r.prasaptaraya@gmail.com>
2016-04-29 16:40:48 +0700

    update

--- PDAMObj/AppDelegate.m
+++ PDAMObj/AppDelegate.m
@@ -13,6 +13,8 @@
 #import "MenuKiri.h"
 #import "ViewController.h"
 #import "JASidePanelController.h"
+#import "iRate.h"
+#import "ATAppUpdater.h"
 
 @interface AppDelegate ()
 
@@ -23,8 +25,22 @@
 @synthesize window = _window;
 @synthesize viewController = _viewController;
 
++ (void)initialize
+{
+    //set the bundle ID. normally you wouldn't need to do this
+    //as it is picked up automatically from your Info.plist file
+    //but we want to test with an app that's actually on the store
+    [iRate sharedInstance].applicationBundleID = @"com.reisap.pdam";
+    [iRate sharedInstance].onlyPromptIfLatestVersion = NO;
+    
+    //enable preview mode
+    [iRate sharedInstance].previewMode = YES;
+}
+
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     [NSThread sleepForTimeInterval:3];
+     [[ATAppUpdater sharedUpdater] showUpdateWithConfirmation];
+   
     [MagicalRecord setupAutoMigratingCoreDataStack];
     UILocalNotification *locationNotification = [launchOptions objectForKey:UIApplicationLaunchOptionsLocalNotificationKey];
     if (locationNotification) {
